1b5b5e2d91815ab57d339573223303ffc889e926
OussamaTh <touhamioussama2206@gmail.com>
2026-07-05 15:03:54 +0100

    dd

--- api/index.php
+++ api/index.php
@@ -1,4 +1,8 @@
 <?php
 
-// Forward Vercel requests to the normal Laravel public/index.php
+// Dynamically build the required folders inside Vercel's writable /tmp directory
+if (!is_dir('/tmp/storage/framework/views')) {
+    mkdir('/tmp/storage/framework/views', 0755, true);
+}
+
 require __DIR__ . '/../public/index.php';
