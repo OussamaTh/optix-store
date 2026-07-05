f0946b70d55baa63e4ddb91f9b27341ab797ee47
OussamaTh <touhamioussama2206@gmail.com>
2026-07-05 20:01:25 +0100

    fixed the image_path error

--- resources/views/partials/cart-item.blade.php
+++ resources/views/partials/cart-item.blade.php
@@ -1,6 +1,6 @@
 <div class="flex gap-4 cart-item-row" data-cart-item-id="{{ $item->id }}">
     <div class="w-16 h-16 rounded-xl bg-gray-50 border border-[#19140010] overflow-hidden shrink-0">
-        @if ($item->product->image_path)
+        @if ($item->product?->image_path)
             <img src="{{ \Illuminate\Support\Facades\Storage::url($item->product->image_path) }}"
                 alt="{{ $item->product->name }}" class="w-full h-full object-cover">
         @endif
